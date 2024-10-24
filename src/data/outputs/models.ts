25bc32238a1cef50558575bed67368c7e906e572
Tanuj <Tanuj@cmu.edu>
2024-10-24 16:29:10 -0700

    Add YML and TS sample data

---
+++ src/data/outputs/models.ts
@@ -0,0 +1,167 @@
+
+export const LMSTUDIO_LLAMA_1B: Model = {
+    id: "openai/llama-3.2-1b-instruct",
+    updated_at: "2024-10-24T15:45:20.231378",
+    created_at: "2024-10-24T15:45:20.231395",
+    name: "lmstudio-Llama-1B",
+    prompt: "",
+    option: "openai/llama-3.2-1b-instruct",
+    temperature: "0",
+    pinned: false,
+    vision: false,
+    base_url: "http://localhost:4000/v1",
+};
+
+
+export const QWEN2_5_72B: Model = {
+    id: "openai/Qwen/Qwen2.5-72B-Instruct-Turbo",
+    updated_at: "2024-10-24T15:45:20.231397",
+    created_at: "2024-10-24T15:45:20.231398",
+    name: "Qwen2.5-72B",
+    prompt: "",
+    option: "openai/Qwen/Qwen2.5-72B-Instruct-Turbo",
+    temperature: "0",
+    pinned: false,
+    vision: false,
+    base_url: "http://localhost:4000/v1",
+};
+
+
+export const GPT_4O: Model = {
+    id: "openai/gpt-4o",
+    updated_at: "2024-10-24T15:45:20.231400",
+    created_at: "2024-10-24T15:45:20.231400",
+    name: "gpt-4o",
+    prompt: "",
+    option: "openai/gpt-4o",
+    temperature: "0",
+    pinned: false,
+    vision: false,
+    base_url: "http://localhost:4000/v1",
+};
+
+
+export const CLAUDE_3_5_SONNET: Model = {
+    id: "openai/anthropic/claude-3.5-sonnet:beta",
+    updated_at: "2024-10-24T15:45:20.231402",
+    created_at: "2024-10-24T15:45:20.231403",
+    name: "claude-3.5-sonnet",
+    prompt: "",
+    option: "openai/anthropic/claude-3.5-sonnet:beta",
+    temperature: "0",
+    pinned: false,
+    vision: false,
+    base_url: "http://localhost:4000/v1",
+};
+
+
+export const LLAMA_3_1_405B: Model = {
+    id: "openai/meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo",
+    updated_at: "2024-10-24T15:45:20.231404",
+    created_at: "2024-10-24T15:45:20.231405",
+    name: "Llama-3.1-405B",
+    prompt: "",
+    option: "openai/meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo",
+    temperature: "0",
+    pinned: false,
+    vision: false,
+    base_url: "http://localhost:4000/v1",
+};
+
+
+export const LLAMA_3_1_70B: Model = {
+    id: "openai/meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo",
+    updated_at: "2024-10-24T15:45:20.231406",
+    created_at: "2024-10-24T15:45:20.231407",
+    name: "Llama-3.1-70B",
+    prompt: "",
+    option: "openai/meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo",
+    temperature: "0",
+    pinned: false,
+    vision: false,
+    base_url: "http://localhost:4000/v1",
+};
+
+
+export const GEMMA_27B: Model = {
+    id: "openai/google/gemma-2-27b-it",
+    updated_at: "2024-10-24T15:45:20.231408",
+    created_at: "2024-10-24T15:45:20.231409",
+    name: "Gemma-27B",
+    prompt: "",
+    option: "openai/google/gemma-2-27b-it",
+    temperature: "0",
+    pinned: false,
+    vision: false,
+    base_url: "http://localhost:4000/v1",
+};
+
+
+export const LMSTUDIO_QWEN_14B: Model = {
+    id: "openai/qwen2.5-14b-instruct",
+    updated_at: "2024-10-24T15:45:20.231412",
+    created_at: "2024-10-24T15:45:20.231413",
+    name: "lmstudio-qwen-14B",
+    prompt: "",
+    option: "openai/qwen2.5-14b-instruct",
+    temperature: "0",
+    pinned: false,
+    vision: false,
+    base_url: "http://localhost:4000/v1",
+};
+
+
+export const LMSTUDIO_NEMO: Model = {
+    id: "openai/mistral-nemo-instruct-2407",
+    updated_at: "2024-10-24T15:45:20.231415",
+    created_at: "2024-10-24T15:45:20.231415",
+    name: "lmstudio-nemo",
+    prompt: "",
+    option: "openai/mistral-nemo-instruct-2407",
+    temperature: "0",
+    pinned: false,
+    vision: false,
+    base_url: "http://localhost:4000/v1",
+};
+
+
+export const LMSTUDIO_LLAMA_3_2_3B_4BIT_MLX: Model = {
+    id: "openai/llama-3.2-3b-instruct",
+    updated_at: "2024-10-24T15:45:20.231417",
+    created_at: "2024-10-24T15:45:20.231418",
+    name: "lmstudio-Llama-3.2-3B-4bit-MLX",
+    prompt: "",
+    option: "openai/llama-3.2-3b-instruct",
+    temperature: "0",
+    pinned: false,
+    vision: false,
+    base_url: "http://localhost:4000/v1",
+};
+
+
+export const GPT_4O_MINI: Model = {
+    id: "openai/gpt-4o-mini",
+    updated_at: "2024-10-24T15:45:20.231419",
+    created_at: "2024-10-24T15:45:20.231420",
+    name: "gpt-4o-mini",
+    prompt: "",
+    option: "openai/gpt-4o-mini",
+    temperature: "0",
+    pinned: false,
+    vision: false,
+    base_url: "http://localhost:4000/v1",
+};
+
+
+export const QWEN2_5_7B_MLX: Model = {
+    id: "openai/happzy2633-qwen2.5-7b-ins-v3",
+    updated_at: "2024-10-24T15:45:20.231421",
+    created_at: "2024-10-24T15:45:20.231422",
+    name: "Qwen2.5-7B-MLX",
+    prompt: "",
+    option: "openai/happzy2633-qwen2.5-7b-ins-v3",
+    temperature: "0",
+    pinned: false,
+    vision: false,
+    base_url: "http://localhost:4000/v1",
+};
